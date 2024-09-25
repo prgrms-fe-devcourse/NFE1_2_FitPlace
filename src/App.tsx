b25e11700a4f99b2062e76f174d1abf539d28f12
HSCHEOL <knr0013@gmail.com>
2024-09-25 15:46:25 +0900

    feat: Header 분리 후 재결합

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Mainpage from "./pages/Mainpage";
-import Header from "./components/Header";
 import Login from "./pages/register/Login";
 import Register from "./pages/register/Register";
 import Ranking_page from "./pages/Ranking_page";
@@ -10,7 +9,6 @@ import Location from "./pages/LocationSetting";
 const App = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-white">
-      <Header />
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/login" element={<Login />} />
--- src/pages/Ranking_page.tsx
+++ src/pages/Ranking_page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Ranking_page = () => {
-  return <div className="min-h-screen bg-white w-140 overflow-y-hidden"></div>;
+  return (
+    <>
+      <Header />
+      <div className="min-h-screen bg-white w-140 overflow-y-hidden"></div>
+    </>
+  );
 };
 
 export default Ranking_page;
