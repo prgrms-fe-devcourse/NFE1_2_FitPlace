78cf1cc09ab5598a1b023e2d18fd8f920d5bc310
한지원 <gomdeschool@naver.com>
2024-09-25 15:07:52 +0900

    feat:게시글 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import KakaoMap from './KakaoMap'; // KakaoMap 컴포넌트 임포트 관련 코
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Mainpage from "./pages/Mainpage";
+import NotionAdd from './pages/NotionAdd';
 
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <Routes>
-        <Route path="/" element={<Mainpage />} />
+        <Route path="/" element={<NotionAdd />} />
       </Routes> 
     </div>
   );
---
+++ src/pages/NotionAdd.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+
+const NotionAdd = () => {
+
+  return (
+    
+    <div className='bg-white w-640px h-full' >
+      <header>
+        <button><i></i></button>
+        <h1>모임 만들기</h1>
+      </header>
+      <form action="#" className='m-5'>
+        <div >
+          <label htmlFor="meetName" className='flex font-bold text-xl mt-20'>모임 이름</label>
+          <input type="text" id='meetName' placeholder='모임 이름을 입력해주세요.' className='border-2 border-solid border-[#e8e8e8] w-96 mt-2.5 text-lg'/>
+        </div>
+        <div>
+          <label htmlFor="meetPersonnel" className='flex font-bold text-xl mt-6'>모임 인원</label>
+          <input type="text" id='meetPersonnel' placeholder='모임 인원을 입력해주세요' className='border-2 border-solid border-[#e8e8e8] w-96 mt-2.5 text-lg'/>
+        </div>
+        <div>
+          <h3 className='font-bold text-xl mt-6'>운동 종목</h3>
+          <div className='mt-2.5'>
+            <label htmlFor="baseball" className=''>야구</label>
+            <input type='radio' name='sports' id='baseball' value='야구'/>
+            <label htmlFor="soccer">축구</label>
+            <input type='radio' name='sports' id='soccer' value='축구'/>
+            <label htmlFor="tennis">테니스</label>
+            <input type='radio' name='sports' id='tennis' value='테니스'/>
+            <label htmlFor="basketball">농구</label>
+            <input type='radio' name='sports' id='basketball' value='농구'/>
+            <label htmlFor="directInput">직접 입력</label>
+            <input type='radio' name='sports' id='directInput' value='직접 입력'/>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="meetexposition" className='flex font-bold text-xl mt-6'>모임 설명</label>
+          <input type="text" id='meetexposition' placeholder='모임에 대한 설명을 입력해주세요.' className='border-2 border-solid border-[#e8e8e8] w-full h-32 mt-2.5 text-lg'/>
+        </div>
+        <div>
+          <p className='font-bold text-xl mt-6'>운동장소</p>
+          <div className='mt-2.5 border-2 border-solid border-[#e8e8e8] w-96 '>
+            <label htmlFor="selectLocation" className='text-[#656565]'>위치 선택</label>
+            <button id='selectLocation'></button>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="meetImg" className='flex font-bold text-xl mt-6'>사진 등록</label>
+          <input type="file" id='meetImg' className='mt-2.5' />
+        </div>
+      </form>    
+    </div>
+  );
+};
+
+export default NotionAdd;
