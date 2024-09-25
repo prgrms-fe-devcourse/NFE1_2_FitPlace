afb10b9ef9ece3ffb21568830ced13b57ec53030
HSCHEOL <knr0013@gmail.com>
2024-09-25 11:14:32 +0900

    feat: Header 생성

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const Mainpage = () => {
   return (
     <>
-      <div className=" w-140 h-full border-solid border-2 border-neutral-400"></div>
-      <div>메인페이지 퍼블리싱</div>
+      <div className=" w-140 h-full bg-white"></div>
     </>
   );
 };
