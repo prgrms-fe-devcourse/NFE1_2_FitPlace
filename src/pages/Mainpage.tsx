619fa8c4d8fbef8b3e7ce1bc447c81a928e81f87
HSCHEOL <knr0013@gmail.com>
2024-09-25 10:38:16 +0900

    feat:버튼 생성

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Mainpage = () => {
   return (
-    <div className=" w-140 h-full border-solid border-2 border-neutral-400"></div>
+    <>
+      <div className=" w-140 h-full border-solid border-2 border-neutral-400"></div>
+      <div>메인페이지 퍼블리싱</div>
+    </>
   );
 };
 
