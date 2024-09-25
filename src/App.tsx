6d6c0e87ed4fc8efe03622287b738a5803a06159
한지원 <gomdeschool@naver.com>
2024-09-25 17:53:28 +0900

    feat:게시글 생성

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,6 @@ import NotionAdd from './pages/NotionAdd';
 
 
 const App = () => {
-    return (
-        <div className="flex flex-col justify-center items-center min-h-screen bg-white">
-            <Routes>
-                <Route path="/" element={<Mainpage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/ranking" element={<Ranking_page />} />
-                <Route path="/map" element={<Location />} />
-                <Route path="/search" element={<SearchPage />} />
-            </Routes>
-        </div>
-    );
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-white">
       <Routes>
@@ -31,6 +19,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/ranking" element={<Ranking_page />} />
         <Route path="/map" element={<Location />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/notionAdd" element={<NotionAdd />} />
       </Routes> 
     </div>
