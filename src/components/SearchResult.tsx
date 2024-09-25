c6ff1357a58b5cf2d24ff107c8f743cd279c5f39
seon022 <seon022@icloud.com>
2024-09-25 15:52:42 +0900

    feat:검색페이지 카테고리 클릭 스타일

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Health_post from "../components/Health_post";
 import Button from "../components/Button";
 
 const SearchResult = () => {
+    const exerciseList = ["전체", "축구", "야구", "러닝", "자전거"];
+    const [activeButton, setActiveButton] = useState("전체");
+
+    const handleButtonClick = (item: string) => {
+        setActiveButton(item);
+    };
+
     return (
         <div>
-            <div className="postCategory">
-                <Button label="전체" color="green" size="mid" />
-                <Button label="러닝" color="grey" size="mid" />
-                <Button label="풋살" color="grey" size="mid" />
-                <Button label="야구" color="grey" size="mid" />
-                <Button label="테니스" color="grey" size="mid" />
-                <Button label="자전거" color="grey" size="mid" />
+            <div className="mb-10">
+                {exerciseList.map((item, index) => (
+                    <Button
+                        key={index}
+                        label={item}
+                        color={item === activeButton ? "green" : "grey"}
+                        size="mid"
+                        margin="btnMr"
+                        onClick={() => handleButtonClick(item)}
+                    />
+                ))}
             </div>
             <section>
-                <p className="font-bold mt-11 text-xl mb-3">인기 모임</p>
+                <p className="font-bold text-xl mb-3">인기 모임</p>
                 <div className="h-1/2">
                     <Health_post title={"러닝 크루 모집"} />
                     <Health_post title={"배드민턴 치실분"} />
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import Search_bar from "../components/Search_bar";
 import SearchResult from "../components/SearchResult";
 
 const Search = () => {
+    const buttons = ["포스트", "사용자"];
+    const [activeButton, setActiveButton] = useState(buttons[0]);
+
+    const handleButtonClick = (item: string) => {
+        setActiveButton(item);
+    };
+
     return (
         <div className="w-140 min-h-screen bg-white p-3">
             <section className="mb-6">
                 <Search_bar placeholder="검색어를 입력해주세요." />
             </section>
             <div className="mb-4 flex">
-                <Button label="포스트" color="green" size="mid" />
-                <Button label="사용자" color="grey" size="mid" />
+                {buttons.map((button, index) => (
+                    <Button
+                        key={index}
+                        label={button}
+                        color={activeButton === button ? "green" : "grey"}
+                        size="mid"
+                        margin="btnMr"
+                        onClick={() => handleButtonClick(button)}
+                    />
+                ))}
             </div>
             <SearchResult />
         </div>
