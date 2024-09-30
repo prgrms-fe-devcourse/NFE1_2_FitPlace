5b5ffa26e9aae73a8612f0288363b93727b55c5c
HSCHEOL <knr0013@gmail.com>
2024-09-30 14:32:30 +0900

    feat:댓글 페이지 렌더링

--- src/pages/CommentPage.tsx
+++ src/pages/CommentPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CommentItem from "../components/CommentItem";
 import Button from "../components/Button";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import axios from "axios";
 
 // Comment 타입 정의
@@ -14,14 +14,53 @@ interface Comment {
 }
 const CommentPage = (): JSX.Element => {
   const { id } = useParams();
-  const [comment, setComment] = useState();
+  const [Post, setPost] = useState();
+  const [Input_value, setInput_value] = useState("");
+  const [ParsingData, setParsingData] = useState("");
+
+  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0ZWRiYTRkN2M1NGYyMTI4ZTQ2Y2NlNSIsImVtYWlsIjoiYWRtaW5AcHJvZ3JhbW1lcnMuY28ua3IifSwiaWF0IjoxNzI3Mzk3NTY0fQ.ziDMvpbQF6K61P2POdELAiyLocTIMZ7IZGbe8ZiYlqg`;
 
   const CommentLoading = async () => {
     try {
       const response = await axios.get(
         `https://kdt.frontend.5th.programmers.co.kr:5009/posts/${id}`
       );
-      setComment(response.data);
+      setPost(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (Post) {
+      try {
+        const parsedTitle = JSON.parse(Post.title);
+        setParsingData(parsedTitle);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [Post]);
+
+  const Input_comment = (e) => {
+    setInput_value(e.target.value);
+  };
+
+  const Register_comment = async () => {
+    try {
+      await axios.post(
+        `https://kdt.frontend.5th.programmers.co.kr:5009/comments/create`,
+        {
+          comment: Input_value,
+          postId: id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // JWT 토큰 추가
+          },
+        }
+      );
+      setInput_value("");
     } catch (error) {
       console.log(error);
     }
@@ -31,19 +70,28 @@ const CommentPage = (): JSX.Element => {
     CommentLoading();
   }, []);
 
-  useEffect(() => {
-    console.log(comment);
-  }, [comment]);
-
   return (
     <>
       <div className="relative bg-white w-140 mx-auto min-h-screen flex flex-col">
         <div className="flex-grow p-3">
-          <div className="text-sm text-[#AFE327] mb-1 font-bold">모집중!</div>
-          <div className="text-xl font-bold mb-4">풋살 4 vs 4 모집 ⚽</div>
-          <div className="text-sm text-gray-600 mb-8">풋살⚽</div>
-          {comment &&
-            comment.comments.map((item, idx) => (
+          {ParsingData.currentMember === ParsingData.meetingCapacity ? (
+            <div className="text-sm text-rose-600 mb-1 font-bold">
+              모집 마감
+            </div>
+          ) : (
+            <div className="text-sm text-[#AFE327] mb-1 font-bold">모집중</div>
+          )}
+
+          <div className="text-xl font-bold mb-4">
+            {ParsingData.title || "제목 없음"}
+          </div>
+
+          <div className="text-sm text-gray-600 mb-8">
+            {Post && Post.channel.name}
+          </div>
+
+          {Post &&
+            Post.comments.map((item, idx) => (
               <div
                 key={idx}
                 className="bg-gray-100 w-full  rounded-md shadow-lg mb-5 p-4 flex flex-col"
@@ -51,13 +99,20 @@ const CommentPage = (): JSX.Element => {
                 <CommentItem item={item} />
               </div>
             ))}
+
           <div className="absolute bottom-0 left-0 right-0 bg-white border-t border-gray-200 p-6">
             <div className="flex w-full">
               <textarea
                 placeholder="댓글을 입력하세요"
                 className="flex-1 border rounded-[4px] mr-4 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none overflow-hidden"
+                onChange={(e) => Input_comment(e)}
+                value={Input_value}
               />
-              <Button label="등록" size="small" />
+              <Button
+                label="등록"
+                size="small"
+                onClick={() => Register_comment()}
+              />
             </div>
           </div>
         </div>
