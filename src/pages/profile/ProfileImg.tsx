e8c1de01a41d0c7f30742b02bd9f5fb43c555ce4
w-ho-choo <ettoprivate@gmail.com>
2024-10-02 17:47:20 +0900

    feat: 사진 개수 마다 사진 렌더링되게 & 사진 2개 이상이면 업로드 못하도록 할것.(예정)

--- src/pages/profile/ProfileImg.tsx
+++ src/pages/profile/ProfileImg.tsx
@@ -10,16 +10,15 @@ interface UserData {
   userId: string;
   description: string;
   location: string;
-  image: string;
+  image: string[];
 }
 
 const ProfileImg = () => {
-
   const cookie = new Cookies();
   const navigate = useNavigate();
-  
-  let imgUrl = '';
-  const [myToken, setMyToken] = useState('');
+
+  let imgUrl = "";
+  const [myToken, setMyToken] = useState("");
   const [myData, setMyData] = useState<UserData>();
 
   useEffect(() => {
@@ -40,31 +39,42 @@ const ProfileImg = () => {
     }
   }, [cookie]);
 
-
   const uploadImg = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if(!e.target.files) {
-      return
+    // 어레이 개수가 2개 이상이면 불가하게끔 하는거 넣어야 함 10-02
+    if (!e.target.files) {
+      return;
     } else {
       const file = e.target.files[0];
       const formData = new FormData();
-      formData.append('file', file);
-      formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-      formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
-  
+      formData.append("file", file);
+      formData.append(
+        "upload_preset",
+        import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+      );
+      formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY);
+
       try {
-        const response = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUD_NAME}/upload`, formData);
+        const response = await axios.post(
+          `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUD_NAME}/upload`,
+          formData
+        );
         imgUrl = response.data.secure_url;
-        putImg(imgUrl)
-      } catch(err) {
-        console.log(err)
+        putImg(imgUrl);
+      } catch (err) {
+        console.log(err);
       }
     }
-  }
+  };
 
-  const putImg = async (imgUrl: string) => {
-    if(imgUrl || imgUrl !== '') {
+  const putImg = async (imgUrlParams: string) => {
+    if (imgUrlParams || imgUrlParams !== "") {
       const putData = { ...myData };
-      putData.image = imgUrl;
+      if (Array.isArray(putData.image)) {
+        putData.image.push(imgUrlParams);
+      } else {
+        putData.image = [];
+        putData.image.push(imgUrlParams);
+      }
       const submitData = JSON.stringify(putData);
       await axios
         .put(
@@ -86,70 +96,8 @@ const ProfileImg = () => {
         .catch((err) => {
           console.log(err);
         });
-      }
+    }
   };
-
-  // const handleImgUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (!e.target.files) {
-  //     return
-  //   } else {
-  //     const file = e.target.files[0];
-  //     const formData = new FormData();
-  //     formData.append('file', file);
-  //     formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-  //     formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
-  
-  //     try {
-  //       const response = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUD_NAME}/upload`, formData);
-  //       setImgUrl(response.data.secure_url);
-  //     } catch(err) {
-  //       console.log(err)
-  //     }
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   imgUrlToBinary(imgUrl).then((binaryData) => {
-  //     console.log(imgUrl)
-  //     console.log(binaryData)
-  //     if (binaryData) {
-  //       postBinaryImg(binaryData)
-  //     }
-  //   })
-  // }, [imgUrl])
-
-  // const imgUrlToBinary = async (url: string) => {
-  //   try {
-  //     const response = await axios.get(url, { responseType: 'blob' });
-  //     const blob = response.data;
-
-  //     return new Promise((res, rej) => {
-  //       const reader = new FileReader();
-  //       reader.onloadend = () => res(reader.result);
-  //       reader.onerror = rej;
-  //       reader.readAsDataURL(blob);
-  //     })
-  //   } catch (err) {
-  //     console.log(err)
-  //     return null;
-  //   }
-  // }
-
-  // const postBinaryImg = async (binaryData: unknown) => {
-  //   try {
-  //     const response = await axios.post('https://kdt.frontend.5th.programmers.co.kr:5009/users/upload-photo', {
-  //       isCover: false,
-  //       image: binaryData
-  //     }, {
-  //         headers: {
-  //           Authorization: `bearer ${myToken}`
-  //         }
-  //       });
-  //       console.log(response.data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
 
   return (
     <form className="w-140 min-h-screen bg-white p-3 flex flex-col justify-start relative">
@@ -165,15 +113,30 @@ const ProfileImg = () => {
       <div className="edit__head-btm mt-6">
         <ul className="flex justify-start items-start flex-wrap gap-4">
           {/* 이미지 업로드 예시 */}
-          <li className="w-[calc(33.33333%_-_1rem)] relative rounded shadow after:block after:pb-100P">
-            {
-              myData?.image === '' || !myData?.image
-              ? <img src="/src/assets/defaultProfileImg.svg" alt="기본 프로필 사진" className="w-full h-full object-cover absolute" />
-              : <img src={myData?.image} alt={`${myData?.fullName}님의 사진`} className="w-full h-full object-cover absolute" />
-            }
-            {/* 삭제...버튼...? */}
-            <p className="absolute top-0 right-0 cursor-pointer">❌</p>
-          </li>
+          {Array.isArray(myData?.image) &&
+            myData.image.map((img, idx) => {
+              return (
+                <li className="w-[calc(33.33333%_-_1rem)] relative rounded shadow after:block after:pb-100P">
+                  {myData.image.length === 0 ? (
+                    <img
+                      src="/src/assets/defaultProfileImg.svg"
+                      alt="기본 프로필 사진"
+                      className="w-full h-full object-cover absolute"
+                      key={idx}
+                    />
+                  ) : (
+                    <img
+                      src={img}
+                      alt={`${myData?.fullName}님의 사진`}
+                      className="w-full h-full object-cover absolute"
+                      key={idx}
+                    />
+                  )}
+                  {/* 삭제...버튼...? 
+                    <p className="absolute top-0 right-0 cursor-pointer">❌</p> */}
+                </li>
+              );
+            })}
 
           {/* 이미지 업로드 버튼 */}
           <li className="bg-gray-100 hover:bg-gray-200 w-[calc(33.33333%_-_1rem)] relative rounded shadow after:block after:pb-100P">
@@ -193,16 +156,11 @@ const ProfileImg = () => {
             />
           </li>
         </ul>
-        
       </div>
 
       {/* 하단 저장 버튼 */}
       <div className="text-center absolute bottom-8 w-[calc(100%_-_1.5rem)]">
-        <Button
-          label="저장"
-          size="full"
-          color="green"
-        />
+        <Button label="저장" size="full" color="green" />
       </div>
     </form>
   );
--- src/pages/profile/dist/ProfileImg.js
+++ src/pages/profile/dist/ProfileImg.js
@@ -55,8 +55,8 @@ var react_router_dom_1 = require("react-router-dom");
 var ProfileImg = function () {
     var cookie = new react_cookie_1.Cookies();
     var navigate = react_router_dom_1.useNavigate();
-    var imgUrl = '';
-    var _a = react_1.useState(''), myToken = _a[0], setMyToken = _a[1];
+    var imgUrl = "";
+    var _a = react_1.useState(""), myToken = _a[0], setMyToken = _a[1];
     var _b = react_1.useState(), myData = _b[0], setMyData = _b[1];
     react_1.useEffect(function () {
         setMyToken(cookie.get("token").replace(/bearer\s+/g, ""));
@@ -86,9 +86,9 @@ var ProfileImg = function () {
                 case 1:
                     file = e.target.files[0];
                     formData = new FormData();
-                    formData.append('file', file);
-                    formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-                    formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
+                    formData.append("file", file);
+                    formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+                    formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY);
                     _a.label = 2;
                 case 2:
                     _a.trys.push([2, 4, , 5]);
@@ -106,14 +106,20 @@ var ProfileImg = function () {
             }
         });
     }); };
-    var putImg = function (imgUrl) { return __awaiter(void 0, void 0, void 0, function () {
+    var putImg = function (imgUrlParams) { return __awaiter(void 0, void 0, void 0, function () {
         var putData, submitData;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    if (!(imgUrl || imgUrl !== '')) return [3 /*break*/, 2];
+                    if (!(imgUrlParams || imgUrlParams !== "")) return [3 /*break*/, 2];
                     putData = __assign({}, myData);
-                    putData.image = imgUrl;
+                    if (Array.isArray(putData.image)) {
+                        putData.image.push(imgUrlParams);
+                    }
+                    else {
+                        putData.image = [];
+                        putData.image.push(imgUrlParams);
+                    }
                     submitData = JSON.stringify(putData);
                     return [4 /*yield*/, axios_1["default"]
                             .put("https://kdt.frontend.5th.programmers.co.kr:5009/settings/update-user", {
@@ -137,73 +143,16 @@ var ProfileImg = function () {
             }
         });
     }); };
-    // const handleImgUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    //   if (!e.target.files) {
-    //     return
-    //   } else {
-    //     const file = e.target.files[0];
-    //     const formData = new FormData();
-    //     formData.append('file', file);
-    //     formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-    //     formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
-    //     try {
-    //       const response = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUD_NAME}/upload`, formData);
-    //       setImgUrl(response.data.secure_url);
-    //     } catch(err) {
-    //       console.log(err)
-    //     }
-    //   }
-    // }
-    // useEffect(() => {
-    //   imgUrlToBinary(imgUrl).then((binaryData) => {
-    //     console.log(imgUrl)
-    //     console.log(binaryData)
-    //     if (binaryData) {
-    //       postBinaryImg(binaryData)
-    //     }
-    //   })
-    // }, [imgUrl])
-    // const imgUrlToBinary = async (url: string) => {
-    //   try {
-    //     const response = await axios.get(url, { responseType: 'blob' });
-    //     const blob = response.data;
-    //     return new Promise((res, rej) => {
-    //       const reader = new FileReader();
-    //       reader.onloadend = () => res(reader.result);
-    //       reader.onerror = rej;
-    //       reader.readAsDataURL(blob);
-    //     })
-    //   } catch (err) {
-    //     console.log(err)
-    //     return null;
-    //   }
-    // }
-    // const postBinaryImg = async (binaryData: unknown) => {
-    //   try {
-    //     const response = await axios.post('https://kdt.frontend.5th.programmers.co.kr:5009/users/upload-photo', {
-    //       isCover: false,
-    //       image: binaryData
-    //     }, {
-    //         headers: {
-    //           Authorization: `bearer ${myToken}`
-    //         }
-    //       });
-    //       console.log(response.data);
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // }
     return (React.createElement("form", { className: "w-140 min-h-screen bg-white p-3 flex flex-col justify-start relative" },
         React.createElement("div", { className: "edit__head-top" },
             React.createElement("p", { className: "font-bold text-xl" }, "\uD504\uB85C\uD544 \uC0AC\uC9C4\uC744 \uB4F1\uB85D\uD574\uC8FC\uC138\uC694."),
             React.createElement("p", { className: "font-normal text-sm mt-2" }, "\uCD5C\uB300 2\uC7A5\uAE4C\uC9C0 \uB4F1\uB85D\uD560 \uC218 \uC788\uC5B4\uC694.")),
         React.createElement("div", { className: "edit__head-btm mt-6" },
             React.createElement("ul", { className: "flex justify-start items-start flex-wrap gap-4" },
-                React.createElement("li", { className: "w-[calc(33.33333%_-_1rem)] relative rounded shadow after:block after:pb-100P" },
-                    (myData === null || myData === void 0 ? void 0 : myData.image) === '' || !(myData === null || myData === void 0 ? void 0 : myData.image)
-                        ? React.createElement("img", { src: "/src/assets/defaultProfileImg.svg", alt: "\uAE30\uBCF8 \uD504\uB85C\uD544 \uC0AC\uC9C4", className: "w-full h-full object-cover absolute" })
-                        : React.createElement("img", { src: myData === null || myData === void 0 ? void 0 : myData.image, alt: (myData === null || myData === void 0 ? void 0 : myData.fullName) + "\uB2D8\uC758 \uC0AC\uC9C4", className: "w-full h-full object-cover absolute" }),
-                    React.createElement("p", { className: "absolute top-0 right-0 cursor-pointer" }, "\u274C")),
+                Array.isArray(myData === null || myData === void 0 ? void 0 : myData.image) &&
+                    myData.image.map(function (img, idx) {
+                        return (React.createElement("li", { className: "w-[calc(33.33333%_-_1rem)] relative rounded shadow after:block after:pb-100P" }, myData.image.length === 0 ? (React.createElement("img", { src: "/src/assets/defaultProfileImg.svg", alt: "\uAE30\uBCF8 \uD504\uB85C\uD544 \uC0AC\uC9C4", className: "w-full h-full object-cover absolute", key: idx })) : (React.createElement("img", { src: img, alt: (myData === null || myData === void 0 ? void 0 : myData.fullName) + "\uB2D8\uC758 \uC0AC\uC9C4", className: "w-full h-full object-cover absolute", key: idx }))));
+                    }),
                 React.createElement("li", { className: "bg-gray-100 hover:bg-gray-200 w-[calc(33.33333%_-_1rem)] relative rounded shadow after:block after:pb-100P" },
                     React.createElement("label", { htmlFor: "imgUploadInput", className: "w-full h-full absolute flex justify-center items-center cursor-pointer" },
                         React.createElement("p", { className: "text-greenColor font-bold text-xl" }, "+ \uC0AC\uC9C4 \uC5C5\uB85C\uB4DC")),
