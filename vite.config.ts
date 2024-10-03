97f0161bd618c0d1e6e8d608189e875a36ac3d13
seon022 <seon022@icloud.com>
2024-10-03 19:01:50 +0900

    fix: healthPost 모집인원 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    //@ts-ignore
-    tailwindcss(),
-  ],
-  server: {
-    port: 8080, // 서버 포트를 8080으로 설정
-    strictPort: true,
-  },
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        //@ts-ignore
+        tailwindcss(),
+    ],
+    server: {
+        port: 8080, // 서버 포트를 8080으로 설정
+        strictPort: true,
+        // proxy: {
+        //     "/api": {
+        //         target: "https://kdt.frontend.5th.programmers.co.kr:5009", // 실제 API 서버 주소
+        //         changeOrigin: true,
+        //         rewrite: (path) => path.replace(/^\/api/, ""), // 경로 재작성
+        //     },
+        // },
+    },
 });
